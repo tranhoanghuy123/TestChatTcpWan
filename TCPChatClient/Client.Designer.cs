1d6d43599411dc75847d31eec6a7219fbc8949c5
tranhoanghuy123 <106876476+tranhoanghuy123@users.noreply.github.com>
2023-03-08 11:38:42 +0700

    Add files via upload

---
+++ TCPChatClient/Client.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TCPChatClient
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisConnect = new System.Windows.Forms.Button();
+            this.txtMess = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(77, 32);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(195, 20);
+            this.txt_IP.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(77, 58);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(195, 20);
+            this.txtPort.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(292, 48);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(89, 30);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisConnect
+            // 
+            this.btnDisConnect.Location = new System.Drawing.Point(387, 48);
+            this.btnDisConnect.Name = "btnDisConnect";
+            this.btnDisConnect.Size = new System.Drawing.Size(89, 30);
+            this.btnDisConnect.TabIndex = 5;
+            this.btnDisConnect.Text = "DisConnect";
+            this.btnDisConnect.UseVisualStyleBackColor = true;
+            this.btnDisConnect.Click += new System.EventHandler(this.btnDisConnect_Click);
+            // 
+            // txtMess
+            // 
+            this.txtMess.Location = new System.Drawing.Point(77, 102);
+            this.txtMess.Name = "txtMess";
+            this.txtMess.Size = new System.Drawing.Size(304, 20);
+            this.txtMess.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(388, 98);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(88, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(77, 142);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(399, 204);
+            this.txtResult.TabIndex = 8;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMess);
+            this.Controls.Add(this.btnDisConnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_IP);
+            this.Controls.Add(this.label1);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_IP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisConnect;
+        private System.Windows.Forms.TextBox txtMess;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ TCPChatClient/Client.cs
@@ -0,0 +1,53 @@
+﻿using SuperSimpleTcp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPChatClient
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+        private SimpleTcpClient client;
+        private void Client_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            IPAddress[] host = Dns.GetHostAddresses(txt_IP.Text);
+            client = new SimpleTcpClient(host[0], int.Parse(txtPort.Text));
+            client.Connect();
+            client.Events.DataReceived += Events_DataReceived;
+        }
+        public void ShowMess(string mess)
+        {
+            txtResult.Text += "\r\n" + mess;
+        }
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string mess = Encoding.UTF8.GetString(e.Data.Array);
+            ShowMess(mess);
+        }
+
+        private void btnDisConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            client.SendAsync(txtMess.Text);
+        }
+    }
+}
